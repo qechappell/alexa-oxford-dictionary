806f6feb075dee1dd32a3188f3a1913179d16600
U-Q-PC\Q <Q@Q-PC.(none)>
2017-01-18 20:12:44 -0500

    initial constants file

---
+++ js/config/Constants.js
@@ -0,0 +1,19 @@
+
+"use strict";
+
+module.exports = Object.freeze({
+	BASE_URL : 'https://od-api.oxforddictionaries.com/api/v1',
+	//application id from https://developer.oxforddictionaries.com/
+	APP_ID : '',
+	//application key from https://developer.oxforddictionaries.com/
+	APP_KEY : '',
+	//language: 'en' for English
+	LANG : 'en',
+	//DICTIONARY, TRANSLATION, THESAURUS, AND SENTENCE ENDPOINT BASE
+	DICT_BASE : '/entries',
+	//LEMMATRON ENDPOINT BASE
+	LEMMATRON_BASE : '/inflections',
+	//SEARCH ENDPOINT BASE
+	SEARCH_BASE : '/search'
+});
+
